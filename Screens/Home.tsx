8572e51d00947ee8d6285ee24915792496d2d10a
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-06-01 19:50:24 +0200

    feat: react-native-user-identity package added to get user information

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,8 +1,20 @@
 import { StyleSheet } from 'react-native'
 import { Layout, Text } from '@ui-kitten/components';
-import React from 'react'
+// TODO: https://github.com/facebook/react-native/issues/36794 -> Issue pending to be solved
+import RNUserIdentity from 'react-native-user-identity';
+import React, { useEffect } from 'react'
 
 const Home = () => {
+
+  useEffect(() => {
+    const getCurrentUser = async function () {
+      console.log('Before uid');
+      const uid = await RNUserIdentity.getUserId({})
+      console.log('uid: ', uid);
+    };
+    getCurrentUser();
+
+  }, []);
 
   return (
     <Layout>
