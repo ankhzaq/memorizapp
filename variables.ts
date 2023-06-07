43286446f941f4ec2e04bac204f144001da73178
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-06-07 17:00:00 +0100

    feat: variables styles added

---
+++ variables.ts
@@ -0,0 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+const black = '#333333';
+const blue = '#4285F4';
+const green = '#42F4A9';
+const yellow =  '#FFC107';
+const white = '#FFFFFF';
+
+export {
+  black,
+  blue,
+  green,
+  yellow,
+  white,
+};
