b7a46ea31026b925317f0a139d8dd2159ed77fbd
Zaquiel Rodriguez Arce <darthzaq@gmail.com>
2023-06-08 17:00:00 +0100

    feat: hook for useApiService added

---
+++ hooks/useApiService.ts
@@ -0,0 +1,47 @@
+import { firebase } from '../config';
+import { Item, ItemWithId } from '../types/item';
+import { Keyboard } from 'react-native';
+
+export function useApiService() {
+  const dataRef = firebase.firestore().collection('bitacora');
+
+  const getItems = async () => {
+    return new Promise((resolve, reject) => {
+      dataRef
+        .orderBy('createdAt', 'desc')
+        .onSnapshot(
+          querySnapshot => {
+            const data = [];
+            querySnapshot.forEach((doc) => {
+              const item = doc.data()
+              data.push({ id: doc.id, ...item});
+            });
+            resolve(data);
+          }, reject);
+    });
+  }
+
+  const addItem = (data: Item) => {
+    return dataRef.add(data);
+  }
+
+  const removeItem = (id: string) => {
+    return dataRef.doc(id).delete();
+  }
+
+  const updateItem = (id: string, data: ItemWithId) => {
+    dataRef
+      .doc(data.id)
+      .update(data)
+      .then(() => {
+        // release keyboard
+        Keyboard.dismiss();
+      })
+      .catch((error) => {
+        // show an alert in case of error
+        alert(error);
+      });
+  }
+
+  return { getItems, addItem, removeItem, updateItem };
+}
